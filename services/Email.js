461251197fcb1976dd6d2cb8d21c47a3cc6a007f
codeUser2000 <aidahakobyanf12@gmail.com>
2022-12-02 22:13:56 +0400

    confirm activation

--- services/Email.js
+++ services/Email.js
@@ -1,12 +1,12 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.mail.ru",
+    host: "smtp.yandex.com",
     secure:true,
     port:465,
     auth: {
-        user: 'nrenishop@mail.ru',
-        pass: 'bvG8vT0fNRfktqV3DPy9'
+        user: 'nreniShop@yandex.ru',
+        pass: 'mlqyzuyynzoukngv'
     }
 });
 
@@ -15,7 +15,7 @@ class Email {
     static sendActivationEmail(email, token, frontUrl) {
         console.log(email)
         return transporter.sendMail({
-            from: '"Nreni" <nrenishop@mail.ru>',
+            from: '"Nreni" <nreniShop@yandex.ru>',
             to: email,
             subject: 'Complete Registration',
             html: `<a href="${frontUrl}?email=${email}&token=${token}">Complete Registration</a>`
