57b65f4dbd70195b5e6bf38e7b92a5da5bc147df
codeUser2000 <aidahakobyanf12@gmail.com>
2022-12-05 00:19:53 +0400

    new pass

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,7 @@ const router = express.Router();
 router.post('/register', validate(usersRegisterSchema), UsersController.register);
 router.get('/confirm', UsersController.confirm);
 router.post('/login', UsersController.login);
+
 router.post('/forget', UsersController.forgetPass);
 router.post('/newPassword', UsersController.newPassword);
 
