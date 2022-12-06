7e0f120d3bc79a886abffef93f431c5cb3c3c48e
unmatched0 <hovhannisya03@mail.ru>
2022-12-07 00:00:10 +0400

    change Categories.js in models files title for categories name

--- models/Categories.js
+++ models/Categories.js
@@ -1,4 +1,4 @@
-import { DataTypes, Model } from 'sequelize';
+import {DataTypes, Model} from 'sequelize';
 import sequelize from '../services/sequelize';
 
 class Categories extends Model {
@@ -6,20 +6,24 @@ class Categories extends Model {
 }
 
 Categories.init({
-  id: {
-    type: DataTypes.BIGINT.UNSIGNED,
-    primaryKey: true,
-    autoIncrement: true,
-    allowNull: false,
-  },
-  name: {
-    type: DataTypes.ENUM('ring', 'bracelet', 'necklace', 'earring', 'collection'),
-    allowNull: false,
-  },
+    id: {
+        type: DataTypes.BIGINT.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+    },
+    parentId: {
+        type: DataTypes.BIGINT.UNSIGNED,
+        allowNull: true,
+    },
+    title: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
 }, {
-  sequelize,
-  modelName: 'categories',
-  tableName: 'categories',
+    sequelize,
+    modelName: 'categories',
+    tableName: 'categories',
 });
 
 export default Categories;
