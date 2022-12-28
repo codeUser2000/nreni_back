c17ff8b6bd3f08f25c246b778907aeaa47fa84b0
unmatched0 <hovhannisya03@mail.ru>
2022-12-28 20:54:59 +0400

    cartItem and createCartItem

--- controllers/CartController.js
+++ controllers/CartController.js
@@ -36,6 +36,8 @@ class CartController {
             const cartItem = await CartItem.create({
                 cartId, productId, title, description, price, quantity, status
             });
+            console.log(cartItem)
+
             res.json({
                 status: 'ok',
                 cartItem,
